176d8d1a3ae29ed61f20db8aeb8cdbd631522fcc
Ryan Templin <tempjuice@hotmail.com>
2019-01-22 15:30:36 -0600

    Single post pages separated and displaying content.

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <h2><a href='<?php the_permalink() ?>'><?php the_title() ?></a></h2>
     
         <div class='content'>
-            <?php the_content() ?>
+            <?php the_excerpt() ?>
         </div>
     <?php endwhile ?>
 
---
+++ single.php
@@ -0,0 +1,17 @@
+<?php get_header() ?>
+
+<?php if ( have_posts() ) : ?>
+    <?php while( have_posts() ) : the_post() ?> 
+        <div <?php post_class()  ?>>
+            <h2><?php the_title() ?></h2>
+            <div class='content'>
+                <?php the_content() ?>
+            </div>
+        </div>  
+
+    <?php endwhile ?>
+<?php else : ?>
+    <p>Well...unfortunately there's no posts.</p>
+<?php endif ?>
+
+
